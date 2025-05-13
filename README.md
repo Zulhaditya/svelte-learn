fb680f4eb03a9a4eade9bbf3721c6183518efe27
Zulhaditya <zulhaditya@gmail.com>
2025-05-13 04:34:10 -0400

    UPDATE : first component

--- README.md
+++ README.md
@@ -23,6 +23,9 @@ npm run dev
 
 # or start the server and open the app in a new browser tab
 npm run dev -- --open
+
+# via ssh
+npm run dev -- --host 0.0.0.0
 ```
 
 ## Building
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!-- script tag untuk javascript/typescript-->
+<script lang="ts">
+  let name = "Svelte";
+</script>
+
+<!-- gunakan {} untuk menampilkan variabel dan menjalankan javascript -->
+<h1>Belajar {name.toUpperCase()}!</h1>
+<p>
+  Kunjungi <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> untuk lihat
+  dokumentasi lengkapnya
+</p>
